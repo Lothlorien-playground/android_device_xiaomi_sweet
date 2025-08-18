d6c1091d08bb6d7206be49e998b6233f83ae99ed
galadriel1402 <galadriel1402@gmail.com>
2025-08-18 12:47:01 +0800

    sweet: Sort blobs and reorganize files
    * Major cleanup
    
    Change-Id: Icdcf3d066197b5d27fa532a98c992f377e0574b8
    Signed-off-by: galadriel1402 <galadriel1402@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021-2024 The LineageOS Project
+# SPDX-FileCopyrightText: The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -15,9 +15,6 @@ TARGET_OTA_ASSERT_DEVICE := sweet,sweetin
 # Audio
 TARGET_PROVIDES_AUDIO_EXTNS := true
 
-# HIDL
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
-
 # Kernel
 TARGET_KERNEL_CONFIG += vendor/sweet.config
 
@@ -31,14 +28,17 @@ BOARD_SUPER_PARTITION_SIZE := 9126805504
 BOARD_QTI_DYNAMIC_PARTITIONS_SIZE := 9122611200 # (BOARD_SUPER_PARTITION_SIZE - 4194304) 4MiB overhead
 
 # Properties
-TARGET_ODM_PROP += $(DEVICE_PATH)/properties/odm.prop
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop
+TARGET_ODM_PROP += $(DEVICE_PATH)/configs/properties/odm.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/properties/vendor.prop
 
 # Screen density
 TARGET_SCREEN_DENSITY := 440
 
 # Vendor security patch level
 VENDOR_SECURITY_PATCH := 2023-11-01
+
+# VINTF
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 
 # Inherit from proprietary files
 include vendor/xiaomi/sweet/BoardConfigVendor.mk
