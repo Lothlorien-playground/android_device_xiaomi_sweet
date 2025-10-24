1be3ecbc8e2ac8789acd92eedcf75ea1bd9c2ffd
galadriel1402 <galadriel1402@gmail.com>
2025-10-24 10:53:28 +0800

    sweet: libinit: Set ro.product.name and ro.product.vendor.name
    Change-Id: Ie5830e40e9721a9c98b9c55f8e88bbfab6a1cdbc

--- libinit/init_xiaomi_sweet.cpp
+++ libinit/init_xiaomi_sweet.cpp
@@ -15,6 +15,7 @@ static const variant_info_t sweet_info = {
 
     .brand = "Redmi",
     .device = "sweet",
+    .name = "sweet_global",
     .marketname = "Redmi Note 10 Pro",
     .model = "M2101K6G",
     .build_fingerprint = "Redmi/sweet_global/sweet:13/RKQ1.210614.002/V14.0.9.0.TKFMIXM:user/release-keys",
@@ -28,6 +29,7 @@ static const variant_info_t sweetjp_info = {
 
     .brand = "Redmi",
     .device = "sweet",
+    .name = "sweet_global",
     .marketname = "Redmi Note 10 Pro",
     .model = "M2101K6R",
     .build_fingerprint = "Redmi/sweet_global/sweet:13/RKQ1.210614.002/V14.0.9.0.TKFMIXM:user/release-keys",
@@ -41,6 +43,7 @@ static const variant_info_t sweetin_info = {
 
     .brand = "Redmi",
     .device = "sweetin",
+    .name = "sweetin",
     .marketname = "Redmi Note 10 Pro",
     .model = "M2101K6P",
     .build_fingerprint = "Redmi/sweetin/sweetin:13/RKQ1.210614.002/V14.0.1.0.TKFINXM:user/release-keys",
@@ -54,6 +57,7 @@ static const variant_info_t sweetinpro_info = {
 
     .brand = "Redmi",
     .device = "sweetin",
+    .name = "sweetinpro",
     .marketname = "Redmi Note 10 Pro Max",
     .model = "M2101K6I",
     .build_fingerprint = "Redmi/sweetinpro/sweetin:13/RKQ1.210614.002/V14.0.1.0.TKFINXM:user/release-keys",
